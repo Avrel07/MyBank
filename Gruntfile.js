76ac0dd7f2dd89c168aa886481ccffea7ecd9e63
Steeve Audoli <audoli.steeve@gmail.com>
2016-02-03 14:25:31 +0100

    Make some front adjustments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,68 @@
 module.exports = function(grunt){
     grunt.initConfig({
-        less: {
-            compile: {
-                options: {
-                    strictMath: true,
-                    sourceMap: true,
-                    outputSourceFiles: true,
-                    plugins: [
-                        new (require('less-plugin-clean-css'))({advanced:true})
-                    ]
-                },
-                files:[
-                    {
-                        src: "app/Resources/public/less/global.less",
-                        dest: "web/public/css/global.css",
-                        sourceMapURL: "global.css.map",
-                        sourceMapFilename: "web/public/css/global.css.map"
-                    }
-
-                ]
-            }
-        },
         watch:{
             css:{
-                files: 'app/Resources/public/less/*.less',
-                tasks: ['less']
+                files: 'app/Resources/public/scss/*.scss',
+                tasks: ['sass']
+            }
+        },
+        sass:{
+            dist: {
+                options: {
+                    style: 'compressed',
+                    precision:8,
+                    trace:true,
+                    update:true
+
+                },
+                files: {
+                    'web/public/css/global.css': 'app/Resources/public/scss/global.scss'
+                }
+            }
+        },
+        copy:{
+            fonts:{
+              files:[
+                  {
+                      cwd: 'vendor-front/bootstrap-sass/assets/fonts/bootstrap/',
+                      dest:'web/public/fonts/',
+                      flatten:true,
+                      filter: 'isFile',
+                      expand:true,
+                      src:'**'
+                  },
+                  {
+                      cwd: 'vendor-front/font-awesome/fonts/',
+                      dest:'web/public/fonts/',
+                      flatten:true,
+                      filter: 'isFile',
+                      expand:true,
+                      src:'**'
+                  },
+                  {
+                      cwd: 'vendor-front/jquery/dist/',
+                      dest:'web/public/js/',
+                      flatten:true,
+                      filter: 'isFile',
+                      expand:true,
+                      src:'jquery.min.js'
+                  },
+                  {
+                      cwd: 'vendor-front/bootstrap-sass/assets/javascripts/',
+                      dest:'web/public/js/',
+                      flatten:true,
+                      filter: 'isFile',
+                      expand:true,
+                      src:'bootstrap.min.js'
+                  },
+              ]
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 }
 
